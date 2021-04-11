2d1ae3255a3d328746415fe5f7ffa3156a14d16f
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2021-04-12 01:37:28 +0500

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const exphbs = require('express-handlebars')
 const todoRoutes = require('./routes/todos')
 
-PORT = process.env.PORT || 9090
+PORT = process.env.PORT || 8080
 
 const hbs = exphbs.create({
     defaultLayout: 'main',
