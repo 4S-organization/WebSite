b7394e005060b2f01c9524d5bf5bd7a0058c8bea
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2021-04-19 00:07:05 +0500

    update

--- routes/todos.js
+++ routes/todos.js
@@ -1,13 +1,13 @@
 const {Router} = require('express')
 const router = Router()
 
-router.get('/', (req, res) => {
+router.get('89.223.125.156/', (req, res) => {
     res.render('index', {
         title: 'Главная'
     })
 })
 
-router.get('/rockets', (req, res) => {
+router.get('89.223.125.156/rockets', (req, res) => {
     res.render('rockets', {
         title: 'Ракеты'
     })
